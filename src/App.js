7a86285367cbfeaa11cf685854b3fc17d649a34b
Aaryaman Maity <aaryamanmaity@gmail.com>
2022-07-26 20:15:49 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FoodBox from './components/FoodBox';
+
+function App() {
+  return (
+    <>
+      <div className='header'>Pro-nutrition</div>
+      <FoodBox />
+    </>
+  );
+}
+
+export default App;
